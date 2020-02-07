da031417b2ace3a6c898902139bc95d75daba63d
Anay Wadhera <awadhera@berkeley.edu>
2020-02-07 10:17:32 -0800

    fajita: use TARGET_VENDOR_OVERLAY_MODULE
    Change-Id: If2385279403daee526bded0eb6d66e007bdc4354

--- lights/Android.mk
+++ lights/Android.mk
@@ -5,16 +5,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845
 LOCAL_MODULE_TAGS  := optional
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_VENDOR_OVERLAY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_STEM := android.hardware.light@2.0-service
+LOCAL_INIT_RC := android.hardware.light@2.0-service.oneplus_sdm845.rc
 
 LOCAL_SRC_FILES := \
     service.cpp \
     Light.cpp
-
-LOCAL_REQUIRED_MODULES := \
-    android.hardware.light@2.0-service.oneplus_sdm845.rc
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
@@ -27,16 +25,3 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.light@2.0
 
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
-
-LOCAL_SRC_FILES := android.hardware.light@2.0-service.oneplus_sdm845.rc
-
-include $(BUILD_PREBUILT)
