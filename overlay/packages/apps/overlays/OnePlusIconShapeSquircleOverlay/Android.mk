2f23e4faf4ce5c4c8676e5c55d2ca94c25aec5eb
Andrew Thomas <itz63c@statixos.com>
2020-04-27 17:05:31 -0400

    sdm845-common: switch to TARGET_COPY_OUT_VENDOR_OVERLAY
    Change-Id: I746c52da374cc9c51de1aeb1a33f313a5c25ef39

--- overlay/packages/apps/overlays/OnePlusIconShapeSquircleOverlay/Android.mk
+++ overlay/packages/apps/overlays/OnePlusIconShapeSquircleOverlay/Android.mk
@@ -9,6 +9,6 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := OnePlusIconShapeSquircleOverlay
 LOCAL_SDK_VERSION := current
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/overlay/oneplus_shape_squircle
+LOCAL_VENDOR_OVERLAY_MODULE := true
 
 include $(BUILD_PACKAGE)
