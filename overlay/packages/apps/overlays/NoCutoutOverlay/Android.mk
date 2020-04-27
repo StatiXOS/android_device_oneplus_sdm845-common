1991b9f0bb8ac48ac3189b0e2e59a8579a246991
Andrew Thomas <itz63c@statixos.com>
2020-04-27 18:46:18 -0400

    sdm845-common: disable Oneplus' noCutoutOverlay
    Change-Id: I4ca5a2da4adb6c1986d03999f92c9d75de8d87c8

---
+++ overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PACKAGE_NAME := NoCutoutOverlay
+LOCAL_SDK_VERSION := current
+
+LOCAL_MODULE_PATH := $(TARGET_COPY_OUT_VENDOR_OVERLAY)/overlay/NoCutoutOverlay
+
+include $(BUILD_PACKAGE) 
