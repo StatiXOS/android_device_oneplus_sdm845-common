c32643684de5a71694d9d9aac862df5e9ee65e29
Anirudh Gupta <anirudhgupta109@aosip.dev>
2021-01-02 12:16:55 -0500

    [circumsized-edition] [DNM for now] sdm845-common: Introduce DeviceSettings
    - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/28dff8aeaadfc3d9e7c97cd80a7373885ca189a8/DeviceSettings
    
    itz63c @ stx:
    - remove keyhandler stuff, as we handle this ROM-side
    - remove touchscreen gestures because i don't care
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Change-Id: I31a7aadb970f6a8dadf641abbb38a00ff263a423

--- common.mk
+++ common.mk
@@ -24,6 +24,10 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := sdm845
 
+# DeviceSettings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
